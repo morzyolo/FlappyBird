f16f0d48d6c134fcf0a77f408fcfd104c639e832
morz <morz21st@gmail.com>
2023-08-24 18:59:05 +0300

    Changed game over screen

--- Assets/Scripts/UI/EndGameUI.cs
+++ Assets/Scripts/UI/EndGameUI.cs
@@ -6,6 +6,7 @@ public class EndGameUI : MonoBehaviour
 {
 	[SerializeField] private RectTransform _panel;
 
+	[SerializeField] private TMP_Text _gameOverText;
 	[SerializeField] private TMP_Text _currentScore;
 
 	[SerializeField] private Button _restartButton;
@@ -18,23 +19,41 @@ public class EndGameUI : MonoBehaviour
 		_gameResult = gameResult;
 	}
 
-	public void Show()
-	{
-		SetActive(true);
-		_exitButton.onClick.AddListener(ChangeScene);
-		_restartButton.onClick.AddListener(RestartGame);
-	}
-
 	public void Hide()
 	{
 		_exitButton.onClick.RemoveListener(ChangeScene);
 		_restartButton.onClick.RemoveListener(RestartGame);
-		SetActive(false);
+		ButtonsSetActive(false);
 	}
 
-	private void SetActive(bool isActive)
+	public void SetGameOverTextAlpha(float alpha)
 	{
-		_panel.gameObject.SetActive(isActive);
+		Color textColor = _gameOverText.color;
+		textColor.a = alpha;
+		_gameOverText.color = textColor;
+	}
+
+	public void SetPanelPosition(float yPosition)
+	{
+		Vector3 panelPosition = _panel.localPosition;
+		panelPosition.y = yPosition;
+		_panel.localPosition = panelPosition;
+	}
+
+	public RectTransform GetGameOverTextRectTransform() => _gameOverText.rectTransform;
+
+	public RectTransform GetPanelRectTransform() => _panel;
+
+	public void ShowButtons()
+	{
+		ButtonsSetActive(true);
+		_exitButton.onClick.AddListener(ChangeScene);
+		_restartButton.onClick.AddListener(RestartGame);
+	}
+
+
+	private void ButtonsSetActive(bool isActive)
+	{
 		_restartButton.gameObject.SetActive(isActive);
 		_exitButton.gameObject.SetActive(isActive);
 	}
--- Assets/Scripts/UIPresenters/GameResult.cs
+++ Assets/Scripts/UIPresenters/GameResult.cs
@@ -1,6 +1,14 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
 public class GameResult
 {
 	private readonly string _menuSceneName = "Menu";
+	private readonly float _gameOverTextYModifier = 50f;
+	private readonly float _uiSpeed = 1.5f;
+
+	private readonly float _panelShowYPosition = 200f;
+	private readonly float _panelHideYPosition = -1500f;
 
 	private readonly Score _score;
 	private readonly EndGameUI _ui;
@@ -28,15 +36,87 @@ public class GameResult
 
 	public async void ChangeScene() => await _sceneChanger.ChangeSceneAsync(_menuSceneName);
 
-	private void ShowResult()
+	private async void ShowResult()
+	{
+		await ShowGameOverText();
+		await ShowPanel();
+		await IncreaceCurrentScore();
+
+		_ui.ShowButtons();
+	}
+
+	private async Task ShowGameOverText()
+	{
+		RectTransform text = _ui.GetGameOverTextRectTransform();
+		float t = 0;
+
+		Vector3 startTextPosition = text.position;
+
+		while (t < 1)
+		{
+			_ui.SetGameOverTextAlpha(t);
+			Vector3 newTextPosition = startTextPosition;
+			newTextPosition.y += CalculateY(t) * _gameOverTextYModifier;
+			text.position = newTextPosition;
+			t += Time.deltaTime * _uiSpeed;
+
+			await Task.Yield();
+		}
+
+		_ui.SetGameOverTextAlpha(1);
+	}
+
+	private float CalculateY(float x) => 4 * x * (1 - x);
+
+	private async Task ShowPanel()
+	{
+		RectTransform panel = _ui.GetPanelRectTransform();
+
+		Vector3 startPosition = panel.localPosition;
+		startPosition.y = _panelHideYPosition;
+
+		Vector3 endPosition = panel.localPosition;
+		endPosition.y = _panelShowYPosition;
+
+		float t = 0;
+
+		while (t < 1)
+		{
+			panel.localPosition = Vector3.Lerp(startPosition, endPosition, t);
+			t += Time.deltaTime * _uiSpeed;
+
+			await Task.Yield();
+		}
+
+		panel.localPosition = endPosition;
+	}
+
+	private async Task IncreaceCurrentScore()
 	{
 		int currentScore = _score.GetCurrentScore();
 
-		_ui.Show();
-		_ui.SetCurrentScore(currentScore);
+		_ui.SetCurrentScore(0);
+
+		float t = 0;
+		int score = 0;
+		while (score < currentScore)
+		{
+			t += Time.deltaTime;
+			score = CalculateScore(t);
+			_ui.SetCurrentScore(score);
+
+			await Task.Yield();
+		}
 	}
 
-	private void Hide() => _ui.Hide();
+	private int CalculateScore(float x) => (int)(x * x);
+
+	private void Hide()
+	{
+		_ui.SetPanelPosition(_panelHideYPosition);
+		_ui.SetGameOverTextAlpha(0);
+		_ui.Hide();
+	}
 
 	private void Unsub()
 	{
