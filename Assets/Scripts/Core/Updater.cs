517ae0812561f6b4b3f13c3bb70d3d186cb697a7
morz <morz21st@gmail.com>
2023-09-03 13:55:54 +0300

    Added small changes

--- Assets/Scripts/Core/Updater.cs
+++ Assets/Scripts/Core/Updater.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class Updater : MonoBehaviour
 {
-	private readonly List<IUpdateListener> _fixedUpdateListeners = new();
+	private readonly List<IUpdateListener> _updateListeners = new();
 
 	private void Update()
 	{
-		foreach (var listener in _fixedUpdateListeners)
+		foreach (var listener in _updateListeners)
 			listener.Tick(Time.deltaTime);
 	}
 
-	public void AddListener(IUpdateListener listener) => _fixedUpdateListeners.Add(listener);
+	public void AddListener(IUpdateListener listener) => _updateListeners.Add(listener);
 
-	public void RemoveListener(IUpdateListener listener) => _fixedUpdateListeners.Remove(listener);
+	public void RemoveListener(IUpdateListener listener) => _updateListeners.Remove(listener);
 }
