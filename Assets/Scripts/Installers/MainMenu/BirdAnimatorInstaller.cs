13875ae5ddf8f0fc53aa867fc6849a33fde07016
morzyolo <morz21st@gmail.com>
2024-01-27 19:00:27 +0300

    Added bird animator installer

---
+++ Assets/Scripts/Installers/MainMenu/BirdAnimatorInstaller.cs
@@ -0,0 +1,39 @@
+using Bird.Components;
+using Configs;
+using SpriteChangers;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Installers.MainMenu
+{
+	public class BirdAnimatorInstaller : MonoInstaller
+	{
+		[SerializeField] private Image _birdImage;
+		[SerializeField] private BirdConfig _birdConfig;
+
+		public override void InstallBindings()
+		{
+			BindSpriteChanger();
+			BindBirdAnimator();
+		}
+
+		private void BindSpriteChanger()
+		{
+			Container
+				.Bind<ISpriteChanger>()
+				.To<ImageChanger>()
+				.AsSingle()
+				.WithArguments(_birdImage);
+		}
+
+		private void BindBirdAnimator()
+		{
+			Container
+				.BindInterfacesTo<BirdAnimator>()
+				.AsSingle()
+				.WithArguments(_birdConfig.AnimationData)
+				.NonLazy();
+		}
+	}
+}
