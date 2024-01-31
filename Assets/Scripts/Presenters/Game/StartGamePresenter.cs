9ada8bf5c48380e9342b50844650e53ef8a26853
morzyolo <morz21st@gmail.com>
2024-01-31 11:31:59 +0300

    Updated start game presenter

--- Assets/Scripts/Presenters/Game/StartGamePresenter.cs
+++ Assets/Scripts/Presenters/Game/StartGamePresenter.cs
@@ -1,6 +1,6 @@
+using System;
 using Core.StateMachines.Game;
 using Core.StateMachines.Game.States;
-using System;
 using UI.Views.Game;
 
 namespace Presenters.Game
@@ -26,20 +26,13 @@ namespace Presenters.Game
 			_state.OnExited -= Disable;
 		}
 
-		public void NotifyStartGame()
-		{
-			_state.GoToNext();
-		}
-
 		private void Enable()
 		{
 			_view.Show();
-			_view.PlayButtonPressed += NotifyStartGame;
 		}
 
 		private void Disable()
 		{
-			_view.PlayButtonPressed -= NotifyStartGame;
 			_view.Hide();
 		}
 	}
