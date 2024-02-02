91ab1d09f96ec096ff3ae38e036e13d56150fc8e
morzyolo <morz21st@gmail.com>
2024-02-02 20:08:41 +0300

    Added end game viwe

---
+++ Assets/Scripts/UI/Views/Game/EndGameView.cs
@@ -0,0 +1,80 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Extensions;
+using TMPro;
+using UI.Elements;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Views.Game
+{
+	public class EndGameView : MonoBehaviour
+	{
+		public event Action OnRestartButtonPressed;
+		public event Action OnExitButtonPressed;
+
+		[SerializeField] private EndGamePanel _panel;
+		[SerializeField] private TMP_Text _gameOverText;
+		[SerializeField] private float _showGameOverTextDuration;
+
+		[Header("Buttons")]
+		[SerializeField] private Button _restartButton;
+		[SerializeField] private Button _exitButton;
+
+		public async UniTaskVoid Show(int currentScore, int bestScore)
+		{
+			gameObject.SetActive(true);
+
+			await ShowGameOverText();
+
+			await _panel.Show(currentScore, bestScore);
+
+			SetButtonsActive(true);
+		}
+
+		public void Hide()
+		{
+			_panel.Hide();
+
+			Color gameOverColor = _gameOverText.color;
+			gameOverColor.a = 0;
+			_gameOverText.color = gameOverColor;
+
+			SetButtonsActive(false);
+			gameObject.SetActive(false);
+		}
+
+		private void SetButtonsActive(bool isActive)
+		{
+			_restartButton.gameObject.SetActive(isActive);
+			_exitButton.gameObject.SetActive(isActive);
+		}
+
+		private async UniTask ShowGameOverText()
+		{
+			Color endColor = _gameOverText.color;
+			endColor.a = 1;
+
+			await _gameOverText
+				.DOColor(endColor, _showGameOverTextDuration)
+				.ToUniTask();
+		}
+
+		private void NotifyRestartButtonPressed() => OnRestartButtonPressed?.Invoke();
+
+		private void NotifyExitButtonPressed() => OnExitButtonPressed?.Invoke();
+
+		private void OnEnable()
+		{
+			_restartButton.AddListener(NotifyRestartButtonPressed);
+			_exitButton.AddListener(NotifyExitButtonPressed);
+		}
+
+		private void OnDisable()
+		{
+			_restartButton.RemoveListener(NotifyRestartButtonPressed);
+			_exitButton.RemoveListener(NotifyExitButtonPressed);
+		}
+	}
+}
