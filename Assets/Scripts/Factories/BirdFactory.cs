3af1ba6741ef87b384dcc4ef3bc8bf8dc217dc26
morzyolo <morz21st@gmail.com>
2024-02-05 13:14:40 +0300

    Updated bird factory

--- Assets/Scripts/Factories/BirdFactory.cs
+++ Assets/Scripts/Factories/BirdFactory.cs
@@ -1,18 +1,24 @@
+using Bird;
+using Bird.Components;
+using Configs.Bird;
 using UnityEngine;
 
-public class BirdFactory
+namespace Factories
 {
-	private readonly BirdConfig _config;
-
-	public BirdFactory(BirdConfig config)
+	public class BirdFactory
 	{
-		_config = config;
-	}
+		private readonly BirdConfig _config;
 
-	public Bird Create()
-	{
-		var bird = Object.Instantiate(_config.BirdPrefab, _config.StartPosition, Quaternion.identity);
-		bird.Initialize(_config);
-		return bird;
+		public BirdFactory(BirdConfig config)
+		{
+			_config = config;
+		}
+
+		public BirdFacade Create()
+		{
+			var bird = Object.Instantiate(_config.BirdPrefab, _config.StartPosition, Quaternion.identity);
+			bird.GetComponent<BirdInitializer>().Initialize();
+			return bird;
+		}
 	}
 }
